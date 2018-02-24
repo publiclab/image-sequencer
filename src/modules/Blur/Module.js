4091609181ec9c18abd514507559141e75bcdee8
Danielle Shwed <danielleshwed@Danielles-MacBook-Pro-2.local>
2018-02-24 13:26:36 -0500

    added saturation module

--- src/modules/Blur/Module.js
+++ src/modules/Blur/Module.js
@@ -55,7 +55,8 @@
          UI: UI
      }
  }
-=======
+
+/*
 * Blur an Image
 */
 module.exports = function Blur(options,UI){
@@ -63,39 +64,39 @@ module.exports = function Blur(options,UI){
     options.title = "Blur";
     options.description = "Blur an Image";
     options.blur = options.blur || 2
-    
+
     //Tell the UI that a step has been set up
     UI.onSetup(options.step);
     var output;
-    
+
     function draw(input,callback){
-        
+
         // Tell the UI that a step is being drawn
         UI.onDraw(options.step);
-        
+
         var step = this;
-        
+
         function changePixel(r, g, b, a){
             return [r,g,b,a]
         }
-        
+
         function extraManipulation(pixels){
             pixels =  require('./Blur')(pixels,options.blur)
             return pixels
         }
 
         function output(image,datauri,mimetype){
-            
+
             // This output is accessible by Image Sequencer
             step.output = {src:datauri,format:mimetype};
-            
+
             // This output is accessible by UI
             options.step.output = datauri;
-            
+
             // Tell UI that step has been drawn.
             UI.onComplete(options.step);
         }
-        
+
         return require('../_nomodule/PixelManipulation.js')(input, {
             output: output,
             changePixel: changePixel,
@@ -104,7 +105,7 @@ module.exports = function Blur(options,UI){
             image: options.image,
             callback: callback
         });
-        
+
     }
     return {
         options: options,
@@ -113,4 +114,3 @@ module.exports = function Blur(options,UI){
         UI: UI
     }
 }
->>>>>>> upstream/master

2c7ca4966a8d9879ae767e36363a514668cc9e45
Mridul Aggarwal <mridulaggarwal97@gmail.com>
2018-09-12 20:16:37 +0530

    Truncate ndvi value for displaying

--- src/modules/Ndvi/Ui.js
+++ src/modules/Ndvi/Ui.js
@@ -19,6 +19,7 @@ module.exports = function CropModuleUi(step, ui) {
             var yPos = e.pageY - offset.top;
             var ndvi = canvas.getContext('2d').getImageData(xPos, yPos, 1, 1).data[0];
             ndvi = ndvi/127.5 - 1 ;
+            ndvi = ndvi.toFixed(2);
             ndviImage[0].title = "NDVI: " + ndvi;
         });
     }

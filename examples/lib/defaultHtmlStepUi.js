9066c9b787735b1abbe60546436dd572c3f0693a
puneet bindal <puneetbindal8989@gmail.com>
2018-09-29 04:19:01 +0530

    Update defaultHtmlStepUi.js

--- examples/lib/defaultHtmlStepUi.js
+++ examples/lib/defaultHtmlStepUi.js
@@ -189,14 +189,7 @@ function DefaultHtmlStepUi(_sequencer, options) {
         );
   }
     
-  function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  } 
+  
 
   function getPreview() {
     return step.imgElement;

f4e5dff604fbb7b8aa4ee44c9c52469ab7a8ec54
kusiodev <kusiodev@gmail.com>
2018-10-28 17:21:34 +0100

    Added resize test, deleted unused value in blend test (which I've added before)

--- test/modules/image-sequencer.js
+++ test/modules/image-sequencer.js
@@ -189,16 +189,29 @@ test('toCliString() returns the CLI command for the sequence', function(t) {
 });
 
 test('blend returns different output depending on the set offset', function(t) {
-    var blend_2;
-    sequencer.addSteps('test', 'invert', {});
-    sequencer.addSteps('test', 'invert', {});
-    sequencer.addSteps('test', 'blend', {});
-    // because we've added blend before, so instead of -3 we set it to -4
-    sequencer.addSteps('test', 'blend', {'offset': -4});
-    sequencer.run({ mode: 'test' }, function(out) {
-        t.notStrictEqual(out, sequencer.images.test.steps[sequencer.images.test.steps.length - 2].output.src, 'different offsets give different output');
-        t.end();
-    });
+  sequencer.addSteps('test', 'invert', {});
+  sequencer.addSteps('test', 'invert', {});
+  sequencer.addSteps('test', 'blend', {});
+  // because we've added blend before, so instead of -3 we set it to -4
+  sequencer.addSteps('test', 'blend', {'offset': -4});
+  sequencer.run({ mode: 'test' }, function(out) {
+    t.notStrictEqual(out, sequencer.images.test.steps[sequencer.images.test.steps.length - 2].output.src, 'different offsets give different output');
+    t.end();
+  });
+});
+
+test('resize changes the width and height of the image', function(t) {
+  sequencer.addSteps('text', 'resize', {});
+  sequencer.run({mode: 'test'}, function() {
+    var width_before = sequencer.images.test.steps[sequencer.images.test.steps.length - 2].options.step.imgElement.naturalWidth,
+      height_before = sequencer.images.test.steps[sequencer.images.test.steps.length - 2].options.step.imgElement.naturalHeight,
+      width_now = sequencer.images.test.steps[sequencer.images.test.steps.length - 1].options.step.imgElement.naturalWidth,
+      height_now = sequencer.images.test.steps[sequencer.images.test.steps.length - 1].options.step.imgElement.naturalHeight;
+
+      t.notEqual(width_before, width_now, 'width is changed');
+      t.notEqual(height_before, height_now, 'height is changed');
+      t.end();
+  });
 });
 
 test('replaceImage returns false in NodeJS', function(t) {

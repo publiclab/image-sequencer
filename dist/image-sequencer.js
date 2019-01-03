bed0cccf019c873c7df1e036c01c3d0aa694529f
Mridul Aggarwal <mridulaggarwal97@gmail.com>
2019-01-04 00:55:50 +0530

    add color functionality

--- dist/image-sequencer.js
+++ dist/image-sequencer.js
@@ -59890,36 +59890,38 @@ module.exports = exports = function(pixels, options){
 	var ex = options.endX;
 	var ey = options.endY;
   var thickness = Number(options.thickness) || 1;
+  var color = options.color || "0 0 0 255";
+  color = color.split(" ");
   for(var i = 0; i<thickness; i++){
     for(var n = (oy+i)*4*iw + 4*ox ; n < (oy+i)*4*iw + 4*(ex); n = n+4){
-       pixels.data[n] = 0;
-       pixels.data[n+1] = 0;
-       pixels.data[n+2] = 0;
-       pixels.data[n+3] = 255;
+       pixels.data[n] = color[0];
+       pixels.data[n+1] = color[1];
+       pixels.data[n+2] = color[2];
+       pixels.data[n+3] = color[3];
     } 
   }
   for(var i = 0; i<thickness; i++){
     for(var n = (ey-i)*4*iw + 4*ox ; n < (ey-i)*4*iw + 4*(ex); n = n+4){
-       pixels.data[n] = 0;
-       pixels.data[n+1] = 0;
-       pixels.data[n+2] = 0;
-       pixels.data[n+3] = 255;
+       pixels.data[n] = color[0];
+       pixels.data[n+1] = color[1];
+       pixels.data[n+2] = color[2];
+       pixels.data[n+3] = color[3];
     }
   }
   for(var i = 0; i < thickness; i++){  
     for(var n = oy*4*iw + 4*(ox+i) ; n < ey*4*iw + 4*(ox+i); n = n+ 4*iw){
-       pixels.data[n] = 0;
-       pixels.data[n+1] = 0;
-       pixels.data[n+2] = 0;
-       pixels.data[n+3] = 255;
+       pixels.data[n] = color[0];
+       pixels.data[n+1] = color[1];
+       pixels.data[n+2] = color[2];
+       pixels.data[n+3] = color[3];
     }
   }
   for(var i = 0; i < thickness; i++){
     for(var n = oy*4*iw + 4*(ex - i) ; n < ey*4*iw + 4*(ex - i); n = n+ 4*iw){
-       pixels.data[n] = 0;
-       pixels.data[n+1] = 0;
-       pixels.data[n+2] = 0;
-       pixels.data[n+3] = 255;
+       pixels.data[n] = color[0];
+       pixels.data[n+1] = color[1];
+       pixels.data[n+2] = color[2];
+       pixels.data[n+3] = color[3];
     }
   }
     return pixels;
@@ -60005,6 +60007,12 @@ module.exports={
         "type": "integer",
         "desc": "thickness of border",
         "default": 1
+      },
+
+      "color":{
+        "type": "String",
+        "desc": "RGBA values separated by a space",
+        "default": "0 0 0 255"
       }
     },
 }

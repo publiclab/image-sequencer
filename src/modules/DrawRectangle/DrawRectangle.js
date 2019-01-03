67b41dd7ba5a97fd93db77e181bd3dc0c57278a5
Mridul Aggarwal <mridulaggarwal97@gmail.com>
2019-01-04 00:45:35 +0530

    added border thickness functionality

--- src/modules/DrawRectangle/DrawRectangle.js
+++ src/modules/DrawRectangle/DrawRectangle.js
@@ -9,29 +9,38 @@ module.exports = exports = function(pixels, options){
 	options.endY = Number(options.endY) || ih - 1;
 	var ex = options.endX;
 	var ey = options.endY;
-    for(var n = oy*4*iw + 4*ox ; n < oy*4*iw + 4*(ex); n = n+4){
+  var thickness = Number(options.thickness) || 1;
+  for(var i = 0; i<thickness; i++){
+    for(var n = (oy+i)*4*iw + 4*ox ; n < (oy+i)*4*iw + 4*(ex); n = n+4){
        pixels.data[n] = 0;
        pixels.data[n+1] = 0;
        pixels.data[n+2] = 0;
        pixels.data[n+3] = 255;
     } 
-    for(var n = ey*4*iw + 4*ox ; n < ey*4*iw + 4*(ex); n = n+4){
+  }
+  for(var i = 0; i<thickness; i++){
+    for(var n = (ey-i)*4*iw + 4*ox ; n < (ey-i)*4*iw + 4*(ex); n = n+4){
        pixels.data[n] = 0;
        pixels.data[n+1] = 0;
        pixels.data[n+2] = 0;
        pixels.data[n+3] = 255;
     }
-    for(var n = oy*4*iw + 4*ox ; n < ey*4*iw + 4*ox; n = n+ 4*iw){
+  }
+  for(var i = 0; i < thickness; i++){  
+    for(var n = oy*4*iw + 4*(ox+i) ; n < ey*4*iw + 4*(ox+i); n = n+ 4*iw){
        pixels.data[n] = 0;
        pixels.data[n+1] = 0;
        pixels.data[n+2] = 0;
        pixels.data[n+3] = 255;
     }
-    for(var n = oy*4*iw + 4*ex ; n < ey*4*iw + 4*ex; n = n+ 4*iw){
+  }
+  for(var i = 0; i < thickness; i++){
+    for(var n = oy*4*iw + 4*(ex - i) ; n < ey*4*iw + 4*(ex - i); n = n+ 4*iw){
        pixels.data[n] = 0;
        pixels.data[n+1] = 0;
        pixels.data[n+2] = 0;
        pixels.data[n+3] = 255;
     }
+  }
     return pixels;
 }

439267d2b075ccacb7e215374db487a5fb46b119
bhavayAnand9 <bhavayanandcse@gmail.com>
2019-01-06 18:19:12 +0530

    brightness module not working on default value 175

--- src/modules/Brightness/Module.js
+++ src/modules/Brightness/Module.js
@@ -1,56 +1,49 @@
 /*
-* Changes the Image Brightness
-*/
+ * Changes the Image Brightness
+ */
 
-module.exports = function Brightness(options,UI){
+module.exports = function Brightness(options, UI) {
+  var output;
 
+  function draw(input, callback, progressObj) {
+    options.brightness = parseInt(options.brightness) || 175;
+    var val = options.brightness / 100.0;
+    progressObj.stop(true);
+    progressObj.overrideFlag = true;
 
-    var output;
-
-    function draw(input,callback,progressObj){
-
-        options.brightness = parseInt(options.brightness) || 100;
-        var val = (options.brightness)/100.0;
-        progressObj.stop(true);
-        progressObj.overrideFlag = true;
-
-        /*
+    /*
         In this case progress is handled by changepixel internally otherwise progressObj
         needs to be overriden and used
         For eg. progressObj = new SomeProgressModule()
         */
 
-        var step = this;
+    var step = this;
 
-        function changePixel(r, g, b, a){
-
-            r = Math.min(val*r, 255)
-            g = Math.min(val*g, 255)
-            b = Math.min(val*b, 255)
-            return [r, g, b, a]
-        }
-
-        function output(image,datauri,mimetype){
-
-            // This output is accessible by Image Sequencer
-            step.output = {src:datauri,format:mimetype};
-
-        }
-
-        return require('../_nomodule/PixelManipulation.js')(input, {
-            output: output,
-            changePixel: changePixel,
-            format: input.format,
-            image: options.image,
-            inBrowser: options.inBrowser,
-            callback: callback
-        });
-
+    function changePixel(r, g, b, a) {
+      r = Math.min(val * r, 255);
+      g = Math.min(val * g, 255);
+      b = Math.min(val * b, 255);
+      return [r, g, b, a];
     }
-    return {
-        options: options,
-        draw:  draw,
-        output: output,
-        UI: UI
+
+    function output(image, datauri, mimetype) {
+      // This output is accessible by Image Sequencer
+      step.output = { src: datauri, format: mimetype };
     }
-}
+
+    return require("../_nomodule/PixelManipulation.js")(input, {
+      output: output,
+      changePixel: changePixel,
+      format: input.format,
+      image: options.image,
+      inBrowser: options.inBrowser,
+      callback: callback
+    });
+  }
+  return {
+    options: options,
+    draw: draw,
+    output: output,
+    UI: UI
+  };
+};

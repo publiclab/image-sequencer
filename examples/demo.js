757d9bb6012b906020abf8de76c5ac3ad5c7f9bd
divy123 <lit2017001@iiitl.ac.in>
2019-01-11 03:47:43 +0530

    Adds thumbnail to insert step view

--- examples/demo.js
+++ examples/demo.js
@@ -213,10 +213,12 @@ window.onload = function() {
     onLoad: function onFileReaderLoad(progress) {
       var reader = progress.target;
       var step = sequencer.images.image1.steps[0];
+      var util=IntermediateHtmlStepUi(sequencer);
       step.output.src = reader.result;
       sequencer.run({ index: 0 });
       step.options.step.imgElement.src = reader.result;
       updatePreviews(reader.result);
+      util.updateThumbnails();
     },
     onTakePhoto: function (url) {
       var step = sequencer.images.image1.steps[0];

c6f10c9bedd642ed71f781c049896a39d5717529
Slytherin <31225007+Divy123@users.noreply.github.com>
2019-01-12 01:36:42 +0530

    Update notification.js

--- examples/lib/notification.js
+++ examples/lib/notification.js
@@ -1,9 +1,10 @@
-function produceNotification(msg) {
-    console.log(msg);
-    if ($('.notification-styles').length == 0) {
+function produceNotification(msg,id) {
+    var id1 ="#"+id; 
+    if ($(id1).length == 0) {
       var notification = document.createElement('span');
       notification.innerHTML = ' <i class="fa fa-info-circle" aria-hidden="true"></i> ' + msg ;
       notification.classList.add("notification-styles");
+      notification.id = id;
   
       $('body').append(notification);
     }

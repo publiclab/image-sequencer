5309b55d78358f32bad1569060cd95ce30d9c461
HarshKhandeparkar <mjkgoa@gmail.com>
2019-01-19 15:53:59 +0530

    chnage thickness

--- src/modules/DrawRectangle/DrawRectangle.js
+++ src/modules/DrawRectangle/DrawRectangle.js
@@ -5,19 +5,19 @@ module.exports = exports = function(pixels, options){
 	  oy = Number(options.startingY),
 	  iw = pixels.shape[0],
 	  ih = pixels.shape[1],
-	  ex =  options.endX = Number(options.endX) || iw - 1,
-	  ey = options.endY = Number(options.endY) || ih - 1,
     thickness = Number(options.thickness) || 1,
+	  ex =  options.endX = Number(options.endX) - thickness || iw - 1,
+	  ey = options.endY = Number(options.endY) -thickness || ih - 1,
     color = options.color || "0 0 0 255";
     color = color.split(" ");
 
   var drawSide = function(startX, startY, endX, endY){
     for (var n=startX; n <= endX+thickness; n++){
       for (var k=startY; k <= endY+thickness; k++){
-        pixels.set(n, k, 0, 255);
-        pixels.set(n, k, 1, 255);
-        pixels.set(n, k, 2, 255);
-        pixels.set(n, k, 3, 255);
+        pixels.set(n, k, 0, color[0]);
+        pixels.set(n, k, 1, color[1]);
+        pixels.set(n, k, 2, color[2]);
+        pixels.set(n, k, 3, color[3]);
       }
     }
   }

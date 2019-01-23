eb46805eebe55a6809cd6e3464ceef1ee2237336
Harsh Prabhu <33490577+harsh1599@users.noreply.github.com>
2019-01-23 21:49:00 +0530

    Updated defaultHtmlStepUI.js

--- examples/lib/defaultHtmlStepUi.js
+++ examples/lib/defaultHtmlStepUi.js
@@ -34,9 +34,11 @@ function DefaultHtmlStepUi(_sequencer, options) {
       '</i></p></div>\
     </div>\
     </form>\
-    <div class="col-md-8 cal">\
-    <div class="load" style="display:none;"><i class="fa fa-circle-o-notch fa-spin"></i></div>\
-    <a><img alt="" style="max-width=100%" class="img-thumbnail step-thumbnail"/></a>\
+    <div class="col-md-8 cal crop-column">\
+      <div class="load" style="display:none;"><i class="fa fa-circle-o-notch fa-spin"></i></div>\
+      <div class = "crop-image">
+        <a><img alt="" style="max-width=100%" class="img-thumbnail step-thumbnail"/></a>\
+      </div>
     </div>\
     </div>\
     </div>\

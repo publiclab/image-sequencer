285a86aa2104f21b972ff53f50a8cae2efa4d3c3
Harsh Khandeparkar <34770591+HarshKhandeparkar@users.noreply.github.com>
2019-04-24 12:42:25 +0530

    Correct bounds
    Co-Authored-By: aashna27 <aashna.mittal27@gmail.com>

--- src/modules/GridOverlay/GridOverlay.js
+++ src/modules/GridOverlay/GridOverlay.js
@@ -15,7 +15,7 @@ module.exports = exports = function(pixels, options){
             }
         }
     
-        for(var y = 0; y <= pixels.shape[1]; y+=options.y){
+        for(var y = 0; y < pixels.shape[1]; y+=options.y){
             for(var x = 0 ; x <= pixels.shape[0]; x++){
                 pixels.set(x, y, 0, color[0]);
                 pixels.set(x, y, 1, color[1]);

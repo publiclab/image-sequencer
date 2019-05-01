6929d4e8dc544ed664e9f87a6e6612031d916446
Harsh <mjkgoa@gmail.com>
2019-05-01 13:07:06 +0530

    remove old commented code

--- src/modules/Convolution/Convolution.js
+++ src/modules/Convolution/Convolution.js
@@ -1,35 +1,11 @@
 var _ = require('lodash');
 module.exports = exports = function(pixels, constantFactor, kernelValues, texMode) {
-  let kernel = kernelGenerator(constantFactor, kernelValues);
-  
-  // const  oldpix = _.cloneDeep(pixels);
-  // kernel = flipKernel(kernel);
-  // for (let i = 0; i < pixels.shape[0]; i++) {
-  //   for (let j = 0; j < pixels.shape[1]; j++) {
-  //     let neighboutPos = getNeighbouringPixelPositions([i, j]);
-  //     let acc = [0.0, 0.0, 0.0, 0.0];
-  //     for (let a = 0; a < kernel.length; a++) {
-  //       for (let b = 0; b < kernel.length; b++) {
-  //         acc[0] += (oldpix.get(neighboutPos[a][b][0], neighboutPos[a][b][1], 0) * kernel[a][b]);
-  //         acc[1] += (oldpix.get(neighboutPos[a][b][0], neighboutPos[a][b][1], 1) * kernel[a][b]);
-  //         acc[2] += (oldpix.get(neighboutPos[a][b][0], neighboutPos[a][b][1], 2) * kernel[a][b]);
-  //         acc[3] += (oldpix.get(neighboutPos[a][b][0], neighboutPos[a][b][1], 3) * kernel[a][b]);
-  //       }
-  //     }
-  //     acc[0] = Math.min(acc[0], 255);
-  //     acc[1] = Math.min(acc[1], 255);
-  //     acc[2] = Math.min(acc[2], 255);
-  //     pixels.set(i, j, 0, acc[0]);
-  //     pixels.set(i, j, 1, acc[1]);
-  //     pixels.set(i, j, 2, acc[2]);
-  //   }
-  // }
-  // return pixels;
-  let pixs = {
-    r: [],
-    g: [],
-    b: [],
-  }
+  let kernel = kernelGenerator(constantFactor, kernelValues),
+    pixs = {
+      r: [],
+      g: [],
+      b: [],
+    }
 
   for (let y = 0; y < pixels.shape[1]; y++){
     pixs.r.push([])
@@ -72,27 +48,3 @@ function kernelGenerator(constantFactor, kernelValues) {
   }
   return arr;
 }
-//   function getNeighbouringPixelPositions(pixelPosition) {
-//     let x = pixelPosition[0], y = pixelPosition[1], result = [];
-
-//     for (let i = -1; i <= 1; i++) {
-//       let arr = [];
-//       for (let j = -1; j <= 1; j++)
-//         arr.push([x + i, y + j]);
-
-//       result.push(arr);
-//     }
-//     return result;
-//   }
-
-//   function flipKernel(kernel) {
-//     let result = [];
-//     for (let i = kernel.length - 1; i >= 0; i--) {
-//       let arr = [];
-//       for (let j = kernel[i].length - 1; j >= 0; j--) {
-//         arr.push(kernel[i][j]);
-//       }
-//       result.push(arr);
-//   }
-//     return result;
-//   }

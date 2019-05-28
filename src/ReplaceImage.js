b1b649be9d8daef5fbf140e46b5e3c80b4056099
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-28 16:14:03 +0300

    Update ReplaceImage.js
    Added const $ = windw.$ at line 4 in ReplaceImage()

--- src/ReplaceImage.js
+++ src/ReplaceImage.js
@@ -1,6 +1,7 @@
 // Uses a given image as input and replaces it with the output.
 // Works only in the browser.
 function ReplaceImage(ref,selector,steps,options) {
+  const $ = window.$;
   if(!ref.options.inBrowser) return false; // This isn't for Node.js
   var tempSequencer = ImageSequencer({ui: false});
   var this_ = ref;

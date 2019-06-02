896d7a7e149a7d631ac47b91f05e48414ea91de6
aashna27 <aashna.mittal27@gmail.com>
2019-06-03 01:03:41 +0530

    Added Color Picker for selecting RGBA values in modules

--- src/modules/GridOverlay/GridOverlay.js
+++ src/modules/GridOverlay/GridOverlay.js
@@ -4,7 +4,8 @@ module.exports = exports = function(pixels, options){
   options.x = Number(options.x) || defaults.x;
   options.y = Number(options.y) || defaults.y;
   color = options.color || defaults.color;
-  color = color.split(' ');
+  color = color.substring(color.indexOf('(')+1,color.length-1); // extract only the values from rgba(_,_,_,_)
+  color = color.split(',');
 
   for(var x = 0; x < pixels.shape[0]; x += options.x){
     for(var y = 0 ; y < pixels.shape[1]; y++){

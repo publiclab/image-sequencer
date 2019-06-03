2f4066ff60db870dbaa6f1c7b9cdae0df5609d4e
Divy123 <lit2017001@iiitl.ac.in>
2019-06-04 00:33:13 +0530

    Add PixelManipulation web assembly code to browser and node

--- src/modules/_nomodule/PixelManipulation.js
+++ src/modules/_nomodule/PixelManipulation.js
@@ -42,16 +42,50 @@ module.exports = function PixelManipulation(image, options) {
 
     if (options.preProcess) pixels = options.preProcess(pixels); // Allow for preprocessing
 
+    function extraOperation() {
+      var res;
+      if (options.extraManipulation) res = options.extraManipulation(pixels, generateOutput);
+      // there may be a more efficient means to encode an image object,
+      // but node modules and their documentation are essentially arcane on this point
+      function generateOutput() {
+        var chunks = [];
+        var totalLength = 0;
+
+        var r = savePixels(pixels, options.format, {
+          quality: 100
+        });
+
+        r.on('data', function (chunk) {
+          totalLength += chunk.length;
+          chunks.push(chunk);
+        });
+
+        r.on('end', function () {
+          var data = Buffer.concat(chunks, totalLength).toString('base64');
+          var datauri = 'data:image/' + options.format + ';base64,' + data;
+          if (options.output)
+            options.output(options.image, datauri, options.format);
+          if (options.callback) options.callback();
+        });
+      }
+      if (res) {
+        pixels = res;
+        generateOutput();
+      } else if (!options.extraManipulation) generateOutput();
+    }
+
+    if (!options.changePixel) extraOperation();
+
     if (options.changePixel) {
 
       /* Allows for Flexibility
        if per pixel manipulation is not required */
 
       const imports = {
-        env:{
+        env: {
           // paceOp: require('pace').op,
-          consoleLog:console.log,
-          perform: function(x,y){
+          consoleLog: console.log,
+          perform: function (x, y) {
             let pixel = options.changePixel(
               pixels.get(x, y, 0),
               pixels.get(x, y, 1),
@@ -60,7 +94,7 @@ module.exports = function PixelManipulation(image, options) {
               x,
               y
             );
-        
+
             pixels.set(x, y, 0, pixel[0]);
             pixels.set(x, y, 1, pixel[1]);
             pixels.set(x, y, 2, pixel[2]);
@@ -68,62 +102,50 @@ module.exports = function PixelManipulation(image, options) {
           }
         }
       };
-      const inBrowser = (options.inBrowser)?1:0;
-      const test = (process.env.TEST)?1:0;
-      WebAssembly.instantiateStreaming(fetch('./program.wasm'), imports)
-        .then(wasm => {
-          wasm.instance.exports.manipulatePixel(pixels.shape[0],pixels.shape[1],inBrowser, test);
+
+      if (options.inBrowser) {
+        const inBrowser = (options.inBrowser) ? 1 : 0;
+        const test = (process.env.TEST) ? 1 : 0;
+        fetch('./manipulation.wasm').then(response =>
+          response.arrayBuffer()
+        ).then(bytes =>
+          WebAssembly.instantiate(bytes, imports)
+        ).then(results => {
+          results.instance.exports.manipulatePixel(pixels.shape[0], pixels.shape[1], inBrowser, test);
+          extraOperation();
+        }).catch(err => {
+          
+          for (var x = 0; x < pixels.shape[0]; x++) {
+            for (var y = 0; y < pixels.shape[1]; y++) {
+              let pixel = options.changePixel(
+                pixels.get(x, y, 0),
+                pixels.get(x, y, 1),
+                pixels.get(x, y, 2),
+                pixels.get(x, y, 3),
+                x,
+                y
+              );
+
+              pixels.set(x, y, 0, pixel[0]);
+              pixels.set(x, y, 1, pixel[1]);
+              pixels.set(x, y, 2, pixel[2]);
+              pixels.set(x, y, 3, pixel[3]);
+
+            }
+          }
+          extraOperation();
+          // console.log(err)
         });
-
-      // for (var x = 0; x < pixels.shape[0]; x++) {
-      //   for (var y = 0; y < pixels.shape[1]; y++) {
-      //     let pixel = options.changePixel(
-      //       pixels.get(x, y, 0),
-      //       pixels.get(x, y, 1),
-      //       pixels.get(x, y, 2),
-      //       pixels.get(x, y, 3),
-      //       x,
-      //       y
-      //     );
-
-      //     pixels.set(x, y, 0, pixel[0]);
-      //     pixels.set(x, y, 1, pixel[1]);
-      //     pixels.set(x, y, 2, pixel[2]);
-      //     pixels.set(x, y, 3, pixel[3]);
-
-      //     if (!options.inBrowser && !process.env.TEST) pace.op();
-      //   }
-      // }
+      } else {
+        const fs = require('fs');
+        const buf = fs.readFileSync('./manipulation.wasm');
+        WebAssembly.instantiate(buf, imports).then(results => {
+          results.instance.exports.manipulatePixel(pixels.shape[0], pixels.shape[1], inBrowser, test);
+          extraOperation();
+        }).catch(err => {
+          console.log(err);
+        });
+      }
     }
-    // perform any extra operations on the entire array:
-    var res;
-    if (options.extraManipulation) res = options.extraManipulation(pixels, generateOutput);
-    // there may be a more efficient means to encode an image object,
-    // but node modules and their documentation are essentially arcane on this point
-    function generateOutput() {
-      var chunks = [];
-      var totalLength = 0;
-
-      var r = savePixels(pixels, options.format, {
-        quality: 100
-      });
-
-      r.on('data', function (chunk) {
-        totalLength += chunk.length;
-        chunks.push(chunk);
-      });
-
-      r.on('end', function () {
-        var data = Buffer.concat(chunks, totalLength).toString('base64');
-        var datauri = 'data:image/' + options.format + ';base64,' + data;
-        if (options.output)
-          options.output(options.image, datauri, options.format);
-        if (options.callback) options.callback();
-      });
-    }
-    if (res) {
-      pixels = res;
-      generateOutput();
-    } else if (!options.extraManipulation) generateOutput();
   });
 };

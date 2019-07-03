22005c01f72dc473ac64ddc10e7de88e1ee8cfac
Divy123 <lit2017001@iiitl.ac.in>
2019-07-03 17:22:13 +0530

    Use base64-img npm module

--- src/modules/MinifyImage/Module.js
+++ src/modules/MinifyImage/Module.js
@@ -1,44 +1,35 @@
-/*
- * Average all pixel colors
- */
-module.exports = function MinifyImage(options, UI) {
 
+module.exports = function MinifyImage(options, UI) {
   var output;
-  var imageDataURI = require('image-data-uri');
+  var base64Img = require('base64-img');
   const imagemin = require('imagemin');
   const imageminJpegtran = require('imagemin-jpegtran');
   const imageminPngquant = require('imagemin-pngquant');
 
   function draw(input, callback, progressObj) {
-
+    console.log('draw');
     progressObj.stop(true);
     progressObj.overrideFlag = true;
 
     var step = this;
-    let filePath = __dirname + '/images/test.' + input.format;
-    imageDataURI.outputFile(input.src, filePath)
-      .then(res =>{
-        (async () => {
-          var inputPath,
-            inputPath = filePath;
-          const files = await imagemin([inputPath], {
-            destination: __dirname + '/results/',
-            plugins: [
-              imageminJpegtran(),
-              imageminPngquant({
-                quality: [0.6, 0.8]
-              })
-            ]
-          });
-          var destPath = __dirname + '/results/test.' + input.format;
-
-          imageDataURI.encodeFromFile(destPath)
-            .then(res => {
-              output(res, input.format );
-              if(callback) callback();
-            });
-        })();
+    let filePath =  './images/';
+    var returnPath = base64Img.imgSync(input.src, filePath, 'test');
+    (async () => {
+      const files = await imagemin([returnPath], {
+        destination: './results/',
+        plugins: [
+          imageminJpegtran(),
+          imageminPngquant({
+            quality: [0.6, 0.8]
+          })
+        ]
       });
+      var destPath = './results/test.' + input.format;
+      var data = base64Img.base64Sync(destPath);
+      output(data, input.format );
+      if(callback) callback();
+    })();
+
 
     function output( datauri, mimetype) {
 

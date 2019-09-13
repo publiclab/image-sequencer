908ab66dff9c2c03c1aa34b32a55cc541f9b2763
keshav234156 <45951376+keshav234156@users.noreply.github.com>
2019-09-13 21:29:25 +0530

    Update Rotate.js

--- src/modules/Rotate/Rotate.js
+++ src/modules/Rotate/Rotate.js
@@ -20,24 +20,22 @@ module.exports = function Rotate(pixels, pixels2, options, rotate_value, width, 
     pixels2.set(x1, y1, 3, pixels1.get(x2, y2, 3));
   }
 
-
-
   pixels1 = require('ndarray')(new Uint8Array(4 * dimension * dimension).fill(0), [dimension, dimension, 4]);
-
+  //copying all the pixels from image to pixels1
   for (var n = 0; n < pixels.shape[0]; n++){
     for (var m = 0; m < pixels.shape[1]; m++){
       copyPixel(n + height_half, m + width_half, n, m);
     }
   }
-
+  //rotating pixels1
   var bitmap = new imagejs.Bitmap({ width: pixels1.shape[0], height: pixels1.shape[1] });
   bitmap._data.data = pixels1.data;
 
   var rotated = bitmap.rotate({
     degrees: rotate_value,
   });
-
   pixels1.data = rotated._data.data;
+  //cropping extra whitespace
   for (var n = 0; n < pixels2.shape[0]; n++){
     for (var m = 0; m < pixels2.shape[1]; m++){
       copyPixel2(n, m, n + Math.floor(dimension / 2 -  Math.abs(width * cos / 2) - Math.abs(height * sin / 2)) - 1, m + Math.floor(dimension / 2 - Math.abs(height * cos / 2) - Math.abs(width * sin / 2)) - 1);

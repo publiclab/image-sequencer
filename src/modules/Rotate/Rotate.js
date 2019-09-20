6e618b5d72417d38258bf6ca11ad41ac3f71def1
keshav234156 <45951376+keshav234156@users.noreply.github.com>
2019-09-20 23:52:12 +0530

    Update Rotate.js

--- src/modules/Rotate/Rotate.js
+++ src/modules/Rotate/Rotate.js
@@ -6,25 +6,18 @@ module.exports = function Rotate(pixels, pixels2, options, rotate_value, width, 
 
   if (rotate_value % 360 == 0)
     return pixels;
-  function copyPixel(x1, y1, x2, y2){
-    pixels1.set(x1, y1, 0, pixels.get(x2, y2, 0));
-    pixels1.set(x1, y1, 1, pixels.get(x2, y2, 1));
-    pixels1.set(x1, y1, 2, pixels.get(x2, y2, 2));
-    pixels1.set(x1, y1, 3, pixels.get(x2, y2, 3));
-  }
-
-  function copyPixel2(x1, y1, x2, y2){
-    pixels2.set(x1, y1, 0, pixels1.get(x2, y2, 0));
-    pixels2.set(x1, y1, 1, pixels1.get(x2, y2, 1));
-    pixels2.set(x1, y1, 2, pixels1.get(x2, y2, 2));
-    pixels2.set(x1, y1, 3, pixels1.get(x2, y2, 3));
+  function copyPixel(x1, y1, x2, y2,pixel_set,pixel_get){
+    pixel_set.set(x1, y1, 0, pixel_get.get(x2, y2, 0));
+    pixel_set.set(x1, y1, 1, pixel_get.get(x2, y2, 1));
+    pixel_set.set(x1, y1, 2, pixel_get.get(x2, y2, 2));
+    pixel_set.set(x1, y1, 3, pixel_get.get(x2, y2, 3));
   }
 
   pixels1 = require('ndarray')(new Uint8Array(4 * dimension * dimension).fill(0), [dimension, dimension, 4]);
   //copying all the pixels from image to pixels1
   for (var n = 0; n < pixels.shape[0]; n++){
     for (var m = 0; m < pixels.shape[1]; m++){
-      copyPixel(n + height_half, m + width_half, n, m);
+      copyPixel(n + height_half, m + width_half, n, m,pixels1,pixels);
     }
   }
   //rotating pixels1
@@ -38,9 +31,8 @@ module.exports = function Rotate(pixels, pixels2, options, rotate_value, width, 
   //cropping extra whitespace
   for (var n = 0; n < pixels2.shape[0]; n++){
     for (var m = 0; m < pixels2.shape[1]; m++){
-      copyPixel2(n, m, n + Math.floor(dimension / 2 -  Math.abs(width * cos / 2) - Math.abs(height * sin / 2)) - 1, m + Math.floor(dimension / 2 - Math.abs(height * cos / 2) - Math.abs(width * sin / 2)) - 1);
+      copyPixel(n, m, n + Math.floor(dimension / 2 -  Math.abs(width * cos / 2) - Math.abs(height * sin / 2)) - 1, m + Math.floor(dimension / 2 - Math.abs(height * cos / 2) - Math.abs(width * sin / 2)) - 1,pixels2,pixels1);
     }
   }
-
   return pixels2;
 };

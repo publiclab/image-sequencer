5c92ddc9569e362f948973aa052d2ab01cc6ecc2
Sagnik Mandal <sonalisagnik123@gmail.com>
2019-12-06 17:15:25 +0530

    Added Dark Mode using CSS Variables

--- examples/demo.js
+++ examples/demo.js
@@ -6,6 +6,30 @@ var defaultHtmlSequencerUi = require('./lib/defaultHtmlSequencerUi.js'),
   insertPreview = require('./lib/insertPreview.js');
 
 window.onload = function () {
+  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+  const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+      document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+      toggleSwitch.checked = true;
+    }
+    }
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }    
+  }
+
+  toggleSwitch.addEventListener('change', switchTheme, false);
+
   sequencer = ImageSequencer();
 
   function refreshOptions() {

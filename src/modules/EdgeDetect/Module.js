1e30c0d06ebe67eca6f8009f377420d8b4ee3ef5
Mr. X <mjkgoa@gmail.com>
2019-12-08 22:16:52 +0530

    fix edge-detect module file comments

--- src/modules/EdgeDetect/Module.js
+++ src/modules/EdgeDetect/Module.js
@@ -24,14 +24,14 @@ module.exports = function edgeDetect(options, UI) {
     // Blur the image
     const internalSequencer = ImageSequencer({ inBrowser: false, ui: false });
     return internalSequencer.loadImage(input.src, function() {
-      internalSequencer.importJSON([{ 'name': 'blur', 'options': { blur: options.blur } }]); // blurs the image before detecting edges to reduce noise
+      internalSequencer.importJSON([{ 'name': 'blur', 'options': { blur: options.blur } }]); // Blurs the image before detecting edges to reduce noise.
       return internalSequencer.run(function onCallback(internalOutput) {
         require('get-pixels')(internalOutput, function(err, blurPixels) {
           if (err) {
             return;
           }
 
-          // Extra Manipulation function used as an enveloper for applying gaussian blur and Convolution
+          // Extra Manipulation function used as an enveloper for applying gaussian blur and Convolution.
           function changePixel(r, g, b, a) {
             return [(r + g + b) / 3, (r + g + b) / 3, (r + g + b) / 3, a];
           }

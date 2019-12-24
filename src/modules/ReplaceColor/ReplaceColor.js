228711b2c5999ce85d4bbb8c2c2c5316ef7d3b8b
Mr. X <mjkgoa@gmail.com>
2019-12-24 23:41:20 +0530

    fix replaceColor

--- src/modules/ReplaceColor/ReplaceColor.js
+++ src/modules/ReplaceColor/ReplaceColor.js
@@ -22,27 +22,28 @@ module.exports = exports = function(pixels, options){
 
   function isSimilar(r, g, b){
     return ( r >= cr * minFactor &&  r <= cr * maxFactor &&
-                 g >= cg * minFactor &&  g <= cg * maxFactor &&
-                 b >= cb * minFactor &&  b <= cb * maxFactor);
+      g >= cg * minFactor &&  g <= cg * maxFactor &&
+      b >= cb * minFactor &&  b <= cb * maxFactor);
   }
+
   for(var i = 0; i < pixels.shape[0]; i++){
     for(var j = 0; j < pixels.shape[1]; j++){
       var r = pixels.get(i, j, 0),
         g = pixels.get(i, j, 1),
         b = pixels.get(i, j, 2);
+        
       if(isSimilar(r, g, b)){
         if (replaceMethod == 'greyscale'){
           var avg = (r + g + b) / 3;
           pixelSetter(i, j, [avg, avg, avg], pixels);
 
-        }else {
+        } else {
           pixelSetter(i, j, replaceColor, pixels);
 
         }
       }
     }
-    
-    
   }
+
   return pixels;
 };

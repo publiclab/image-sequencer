820cfe907b070187d236a66e456ada13fac6eaf1
Mr. X <mjkgoa@gmail.com>
2019-12-24 23:47:21 +0530

    fix edge-detect

--- src/modules/EdgeDetect/Module.js
+++ src/modules/EdgeDetect/Module.js
@@ -1,3 +1,4 @@
+const Blur = require('../Blur/Blur');
 /*
 * Detect Edges in an Image
 */
@@ -19,42 +20,31 @@ module.exports = function edgeDetect(options, UI) {
 
     var step = this;
 
-    // Blur the image
-    const internalSequencer = ImageSequencer({ inBrowser: false, ui: false });
-    return internalSequencer.loadImage(input.src, function() {
-      internalSequencer.importJSON([{ 'name': 'blur', 'options': { blur: options.blur } }]);
-      return internalSequencer.run(function onCallback(internalOutput) {
-        require('get-pixels')(internalOutput, function(err, blurPixels) {
-          if (err) {
-            return;
-          }
+    // Makes the image greyscale
+    function changePixel(r, g, b, a) {
+      return [(r + g + b) / 3, (r + g + b) / 3, (r + g + b) / 3, a];
+    }
 
-          // Extra Manipulation function used as an enveloper for applying gaussian blur and Convolution
-          function changePixel(r, g, b, a) {
-            return [(r + g + b) / 3, (r + g + b) / 3, (r + g + b) / 3, a];
-          }
+    // Extra Manipulation function used as an enveloper for applying gaussian blur and Convolution
+    function extraManipulation(pixels) {
+      const blurPixels = Blur(pixels, options.blur);
+      return require('./EdgeUtils')(blurPixels, options.highThresholdRatio, options.lowThresholdRatio, options.hysteresis);
+    }
 
-          function extraManipulation() {
-            return require('./EdgeUtils')(blurPixels, options.highThresholdRatio, options.lowThresholdRatio, options.hysteresis);
-          }
+    function output(image, datauri, mimetype, wasmSuccess) {
+      step.output = { src: datauri, format: mimetype, wasmSuccess, useWasm: options.useWasm };
+    }
 
-          function output(image, datauri, mimetype, wasmSuccess) {
-            step.output = { src: datauri, format: mimetype, wasmSuccess, useWasm: options.useWasm };
-          }
-
-          return require('../_nomodule/PixelManipulation.js')(input, {
-            output: output,
-            ui: options.step.ui,
-            changePixel: changePixel,
-            extraManipulation: extraManipulation,
-            format: input.format,
-            image: options.image,
-            inBrowser: options.inBrowser,
-            callback: callback,
-            useWasm: options.useWasm
-          });
-        });
-      });
+    return require('../_nomodule/PixelManipulation.js')(input, {
+      output: output,
+      ui: options.step.ui,
+      changePixel: changePixel,
+      extraManipulation: extraManipulation,
+      format: input.format,
+      image: options.image,
+      inBrowser: options.inBrowser,
+      callback: callback,
+      useWasm: options.useWasm
     });
   }
 

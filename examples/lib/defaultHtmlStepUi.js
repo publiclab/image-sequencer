231f2c1929d15348d5eaa699fce8d8735543c404
ataata107 <ataata107@gmail.com>
2019-12-30 01:23:52 +0530

    Refactored changedimension

--- examples/lib/defaultHtmlStepUi.js
+++ examples/lib/defaultHtmlStepUi.js
@@ -339,9 +339,7 @@ function DefaultHtmlStepUi(_sequencer, options) {
 
     $(function () {
       $('[data-toggle="tooltip"]').tooltip();
-      _sequencer.getImageDimensions(step.imgElement.src, function (dim) {
-        step.ui.querySelector('.' + step.name).attributes['data-original-title'].value = `<div style="text-align: center"><p>Image Width: ${dim.width}<br>Image Height: ${dim.height}</br></div>`;
-      });
+      changeDimension(step);
     })
 
     // Handle the wasm bolt display
@@ -351,6 +349,12 @@ function DefaultHtmlStepUi(_sequencer, options) {
       else $step('.wasm-tooltip').fadeOut();
     }
     else $step('.wasm-tooltip').fadeOut();
+  }
+
+  function changeDimension(step){
+    _sequencer.getImageDimensions(step.imgElement.src, function (dim) {
+      step.ui.querySelector('.' + step.name).attributes['data-original-title'].value = `<div style="text-align: center"><p>Image Width: ${dim.width}<br>Image Height: ${dim.height}</br></div>`;
+    });
   }
 
   function imageHover(step){
@@ -409,7 +413,8 @@ function DefaultHtmlStepUi(_sequencer, options) {
     onRemove: onRemove,
     onDraw: onDraw,
     notify: notify,
-    imageHover: imageHover
+    imageHover: imageHover,
+    changeDimension: changeDimension
   };
 }
 

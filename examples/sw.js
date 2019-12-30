e5c1faf014a9c210119d368d56c4e0eba788db91
Anthony Zhou <anthony.zhou03@gmail.com>
2019-12-30 15:02:51 -0600

    Delete unecessary code

--- examples/sw.js
+++ examples/sw.js
@@ -1,23 +1,10 @@
 const staticCacheName = 'image-sequencer-static-v3.5.1';
-// const versionManagement = require('./lib/versionManagement.js');
 self.addEventListener('install', event => {
   console.log('Attempting to install service worker');
 });
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
-  // let localVersionNumber = versionManagement.getLocalVersionNumber();
-  // staticCacheName += localVersionNumber;
-  //
-  // versionManagement.getLatestVersionNumber(function(latestVersionNumber) {
-  //   if(versionManagement.versionCompare(localVersionNumber, latestVersionNumber) < 0) {
-  //     //the local version is out of date, fetch a new version
-  //     console.log("The current version of image sequencer is out of date.");
-  //   } else {
-  //     console.log("The current version of image sequencer is up to date.");
-  //   }
-  // });
-
   e.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(

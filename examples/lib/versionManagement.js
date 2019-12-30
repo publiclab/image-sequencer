27c7c97492d8edb00b7de701bee7ec3d5f2c73d5
Anthony Zhou <anthony.zhou03@gmail.com>
2019-12-30 11:12:35 -0600

    Fix Codeclimate issues

--- examples/lib/versionManagement.js
+++ examples/lib/versionManagement.js
@@ -13,43 +13,30 @@ function getLatestVersionNumber(callback) {
   request.send();
 }
 
-function versionCompare(v1, v2, options) {
-  var lexicographical = options && options.lexicographical,
-      zeroExtend = options && options.zeroExtend,
-      v1parts = v1.split('.'),
+function versionCompare(v1, v2) {
+  var v1parts = v1.split('.'),
       v2parts = v2.split('.');
 
-  function isValidPart(x) {
-      return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
-  }
-
-  if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+  if (areInvalid(v1parts, v2parts)) {
       return NaN;
   }
 
-  if (zeroExtend) {
-      while (v1parts.length < v2parts.length) v1parts.push("0");
-      while (v2parts.length < v1parts.length) v2parts.push("0");
-  }
+  v1parts = v1parts.map(Number);
+  v2parts = v2parts.map(Number);
 
-  if (!lexicographical) {
-      v1parts = v1parts.map(Number);
-      v2parts = v2parts.map(Number);
-  }
+  return compareVersionStrings(v1parts, v2parts);
+}
 
+function compareVersionStrings(v1parts, v2parts) {
+  var compareDigit;
   for (var i = 0; i < v1parts.length; ++i) {
       if (v2parts.length == i) {
           return 1;
       }
 
-      if (v1parts[i] == v2parts[i]) {
-          continue;
-      }
-      else if (v1parts[i] > v2parts[i]) {
-          return 1;
-      }
-      else {
-          return -1;
+      var compareDigit = compare(v1parts[i], v2parts[i]);
+      if(compareDigit != 0) {
+        break;
       }
   }
 
@@ -57,9 +44,32 @@ function versionCompare(v1, v2, options) {
       return -1;
   }
 
-  return 0;
+  return compareDigit;
 }
 
+function compare(part1, part2) {
+  if (part1 == part2) {
+      return 0;
+  }
+  else if (part1 > part2) {
+      return 1;
+  }
+  else {
+      return -1;
+  }
+}
+
+function areInvalid(v1parts, v2parts) {
+  return !v1parts.every(isValidPart) || !v2parts.every(isValidPart);
+}
+
+function isValidPart(x) {
+    return (/^\d+$/).test(x);
+}
+
+function comparePart(v1Part, v2Part) {
+
+}
 function getLocalVersionNumber() {
   return require('../../package.json').version;
 }

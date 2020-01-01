37979fb631fdd36d1f3226469cdba721f90ace93
Anthony Zhou <anthony.zhou03@gmail.com>
2020-01-01 15:13:15 -0600

    Change URL for getting latest version
    Changed the URL for getting the latest NPM version to be based on the package.json file's attribute for "homepage".

--- examples/lib/versionManagement.js
+++ examples/lib/versionManagement.js
@@ -1,14 +1,15 @@
 // Get the current version number from package.json on GitHub.
 function getLatestVersionNumber(callback) {
+  var homepage = require('../../package.json').homepage;
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
     if(request.readyState == 4 && request.status == 200) {
       var response = JSON.parse(this.responseText);
-      var latestVersionNumber = JSON.parse(atob(response.content)).version;
+      var latestVersionNumber = response.version;
       callback(latestVersionNumber);
     }
   }
-  request.open("GET", "https://api.github.com/repos/publiclab/image-sequencer/contents/package.json", true);
+  request.open("GET", homepage + "/package.json", true);
   request.send();
 }
 

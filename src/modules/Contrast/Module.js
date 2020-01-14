64176e542743572141eff8c1c740ce35840731f8
Firela <rishabhshukla1999@gmail.com>
2020-01-15 01:49:32 +0530

    change if block to Math function

--- src/modules/Contrast/Module.js
+++ src/modules/Contrast/Module.js
@@ -28,8 +28,8 @@ module.exports = function Contrast(options, UI) {
       p *= contrast;
       p += 0.5;
       p *= 255;
-      if (p < 0) p = 0;
-      if (p > 255) p = 255;
+      Math.max(0, p);
+      Math.min(p, 255);
       return p;
     }
 

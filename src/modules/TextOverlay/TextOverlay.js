03aca4122a02a532b94d981f1259788fb5dd6f8b
Firela <rishabhshukla1999@gmail.com>
2020-01-17 19:54:35 +0530

    fix text-overlay working in node

--- src/modules/TextOverlay/TextOverlay.js
+++ src/modules/TextOverlay/TextOverlay.js
@@ -8,12 +8,13 @@ module.exports = exports = function(pixels, options){
   options.color = options.color || defaults.color;
   options.size = options.size || defaults.size;
 
-  var canvas = document.createElement('canvas');
-  canvas.width = pixels.shape[0]; //img.width();
-  canvas.height = pixels.shape[1]; //img.height();
-  var ctx = canvas.getContext('2d');
+  const {createCanvas, createImageData} = require('canvas');
 
-  ctx.putImageData(new ImageData(new Uint8ClampedArray(pixels.data), pixels.shape[0], pixels.shape[1]), 0, 0);
+  const canvas = createCanvas(pixels.shape[0], pixels.shape[1]);
+
+  const ctx = canvas.getContext('2d');
+
+  ctx.putImageData(new createImageData(new Uint8ClampedArray(pixels.data), pixels.shape[0], pixels.shape[1]), 0, 0);
 
   ctx.fillStyle = options.color;
   ctx.font = options.size + 'px ' + options.font;

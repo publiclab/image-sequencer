068439bc5cab6c063592f1c2fa9f3a2763924a93
Rishabh Shukla <42492389+blurry-x-face@users.noreply.github.com>
2020-01-18 00:07:46 +0530

    Update isGif.js

--- src/util/isGif.js
+++ src/util/isGif.js
@@ -1,6 +1,6 @@
 module.exports = function(url){
   if(url.match){
-    const toMatch = url.match(/data:image/(.*);/) || 'png';
+    const toMatch = url.match(/data:image\/(.*);/) || 'png';
     return toMatch[1] === 'gif';
   }else
     return false;

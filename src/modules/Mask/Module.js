6ea1c41757b06b8333404d6c01ef3d9e286fb76a
Firela <rishabhshukla1999@gmail.com>
2020-01-20 19:44:32 +0530

    add mask module

--- src/modules/Mask/Module.js
+++ src/modules/Mask/Module.js
@@ -1,7 +1,7 @@
-module.exports = function Blend(options, UI, util) {
+module.exports = function Mask(options, UI, util) {
   var defaults = require('./../../util/getDefaults.js')(require('./info.json'));
-
   options.offset = options.offset || defaults.offset;
+  options.resize = options.resize || defaults.resize;
 
   var output;
 
@@ -30,44 +30,45 @@ module.exports = function Blend(options, UI, util) {
     const alpha_masking = function(c, a) {
       return (a * c + (255 - a) * c) / 255;
     };
+    const internalSequencer = ImageSequencer({ inBrowser: false, ui: false });
+    internalSequencer.loadImage(priorStep.output.src, function() {
+      internalSequencer.importJSON([{ 'name': 'resize', 'options': { resize: options.resize } }]);
+      internalSequencer.run(function onCallback(internalOutput) {
 
-    getPixels(priorStep.output.src, function(err, pixels) {
-      options.firstImagePixels = pixels;
+        getPixels(internalOutput, function(err, pixels) {
+          options.firstImagePixels = pixels;
 
-      // Convert to runnable code.
-      if (typeof options.func === 'string')
-        eval('options.func = ' + options.func);
+          function changePixel(r2, g2, b2, a2, x, y) {
+            let p = options.firstImagePixels;
+            let r1 = p.get(x, y, 0),
+              g1 = p.get(x, y, 1),
+              b1 = p.get(x, y, 2),
+              a1 = p.get(x, y, 3);
 
-      function changePixel(r2, g2, b2, a2, x, y) {
-        // blend!
-        let p = options.firstImagePixels;
-        let r1 = p.get(x, y, 0),
-          g1 = p.get(x, y, 1),
-          b1 = p.get(x, y, 2),
-          a1 = p.get(x, y, 3);
+            return [alpha_masking(r1, a2), alpha_masking(g1, a2), alpha_masking(b1, a2)];
+          }
 
-        return [alpha_masking(r1, a2), alpha_masking(g1, a2), alpha_masking(b1, a2)];
-      }
+          function output(image, datauri, mimetype, wasmSuccess) {
+            step.output = {
+              src: datauri,
+              format: mimetype,
+              wasmSuccess,
+              useWasm: options.useWasm
+            };
+          }
 
-      function output(image, datauri, mimetype, wasmSuccess) {
-        step.output = {
-          src: datauri,
-          format: mimetype,
-          wasmSuccess,
-          useWasm: options.useWasm
-        };
-      }
-
-      // run PixelManipulatin on second image's pixels
-      return require('../_nomodule/PixelManipulation.js')(input, {
-        output: output,
-        ui: options.step.ui,
-        changePixel: changePixel,
-        format: input.format,
-        image: options.image,
-        inBrowser: options.inBrowser,
-        callback: callback,
-        useWasm: options.useWasm
+          // run PixelManipulatin on second image's pixels
+          return require('../_nomodule/PixelManipulation.js')(input, {
+            output: output,
+            ui: options.step.ui,
+            changePixel: changePixel,
+            format: input.format,
+            image: options.image,
+            inBrowser: options.inBrowser,
+            callback: callback,
+            useWasm: options.useWasm
+          });
+        });
       });
     });
   }

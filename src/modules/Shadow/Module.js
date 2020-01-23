7accf6af2a5026c9ccc9a2e7d9e8509a635cce7e
niravasher <f20171066@pilani.bits-pilani.ac.in>
2020-01-24 01:38:56 +0530

    updated shadow module to receive x and y inputs

--- src/modules/Shadow/Module.js
+++ src/modules/Shadow/Module.js
@@ -10,6 +10,9 @@ module.exports = function canvasResize(options, UI) {
 
   function draw(input, callback, progressObj) {
 
+    options.X_value = parseInt(options.X_value || defaults.X_value);
+    options.Y_value = parseInt(options.Y_value || defaults.Y_value);
+
     progressObj.stop(true);
     progressObj.overrideFlag = true;
 
@@ -17,28 +20,102 @@ module.exports = function canvasResize(options, UI) {
 
     function extraManipulation(pixels) {
       let [w, h] = pixels.shape;
-      let newPixels = require('ndarray')(new Uint8Array(4 * (w + 20) * (h + 20)).fill(0), [(w + 20), (h + 20), 4]);
+      let newPixels = require('ndarray')(new Uint8Array(4 * (w + Math.abs(options.X_value)) * (h + Math.abs(options.Y_value))).fill(0), [(w + Math.abs(options.X_value)), (h + Math.abs(options.Y_value)), 4]);
       let iMax = w,
         jMax = h;
-      for (var k = 0; k < 20; k++) {
-        for (var l = 0; l < (h + 20); l++) {
-          let val = 255 - ((k / 20) * 255);
-          pixelSetter(k, l, [val, val, val, 255], newPixels);
-        }
-      }
-      for (var k = 0; k < (w + 20); k++) {
-        for (var l = 0; l < 20; l++) {
-          if (k < 20 && k < l) {
-            continue;
+      if (options.X_value < 0 && options.Y_value < 0) {
+        for (var k = 0; k < Math.abs(options.X_value); k++) {
+          for (var l = 0; l < (h + Math.abs(options.Y_value)); l++) {
+            let val = 255 - ((k / Math.abs(options.X_value)) * 255);
+            pixelSetter(k, l, [val, val, val, 255], newPixels);
           }
-          let val = 255 - ((l / 20) * 255);
-          pixelSetter(k, l, [val, val, val, 255], newPixels);
+        }
+        for (var k = 0; k < (w + Math.abs(options.X_value)); k++) {
+          for (var l = 0; l < Math.abs(options.Y_value); l++) {
+            if (k < Math.abs(options.X_value) && k < l) {
+              continue;
+            }
+            let val = 255 - ((l / Math.abs(options.Y_value)) * 255);
+            pixelSetter(k, l, [val, val, val, 255], newPixels);
+          }
+        }
+        for (let i = 0; i < iMax && i < w; i++) {
+          for (let j = 0; j < jMax && j < h; j++) {
+            let x = i + Math.abs(options.X_value), y = j + Math.abs(options.Y_value);
+            pixelSetter(x, y, [pixels.get(i, j, 0), pixels.get(i, j, 1), pixels.get(i, j, 2), pixels.get(i, j, 3)], newPixels);
+          }
         }
       }
-      for (let i = 0; i < iMax && i < w; i++) {
-        for (let j = 0; j < jMax && j < h; j++) {
-          let x = i + 20, y = j + 20;
-          pixelSetter(x, y, [pixels.get(i, j, 0), pixels.get(i, j, 1), pixels.get(i, j, 2), pixels.get(i, j, 3)], newPixels);
+
+      else if (options.X_value >= 0 && options.Y_value >= 0) {
+        for (var k = w; k < (w + options.X_value); k++) {
+          for (var l = 0; l < (h + options.Y_value); l++) {
+            let val = (((k - w) / options.X_value) * 255);
+            pixelSetter(k, l, [val, val, val, 255], newPixels);
+          }
+        }
+        for (var k = 0; k < (w + options.X_value); k++) {
+          for (var l = h; l < (h + options.Y_value); l++) {
+            if (k >= w && l >= h && ((k - w) >= (l - h))) {
+              continue;
+            }
+            let val = ((l - h) / options.Y_value * 255);
+            pixelSetter(k, l, [val, val, val, 255], newPixels);
+          }
+        }
+        for (let i = 0; i < iMax && i < w; i++) {
+          for (let j = 0; j < jMax && j < h; j++) {
+            let x = i, y = j;
+            pixelSetter(x, y, [pixels.get(i, j, 0), pixels.get(i, j, 1), pixels.get(i, j, 2), pixels.get(i, j, 3)], newPixels);
+          }
+        }
+      }
+      
+      else if (options.X_value < 0 && options.Y_value >= 0) {
+        for (var k = 0; k < Math.abs(options.X_value); k++) {
+          for (var l = 0; l < (h + options.Y_value); l++) {
+            let val = 255 - ((k / Math.abs(options.X_value)) * 255);
+            pixelSetter(k, l, [val, val, val, 255], newPixels);
+          }
+        }
+        for (var k = 0; k < (w + Math.abs(options.X_value)); k++) {
+          for (var l = h; l < (h + options.Y_value); l++) {
+            if (l < (h - ((options.Y_value / options.X_value) * (k - options.X_value))) && k <= options.X_value && l >= h) {
+              continue;
+            }
+            let val = ((l - h) / options.Y_value * 255);
+            pixelSetter(k, l, [val, val, val, 255], newPixels);
+          }
+        }
+        for (let i = 0; i < iMax && i < w; i++) {
+          for (let j = 0; j < jMax && j < h; j++) {
+            let x = i + Math.abs(options.X_value), y = j;
+            pixelSetter(x, y, [pixels.get(i, j, 0), pixels.get(i, j, 1), pixels.get(i, j, 2), pixels.get(i, j, 3)], newPixels);
+          }
+        }
+      }
+      
+      else if (options.X_value >= 0 && options.Y_value < 0) {
+        for (var k = w; k < (w + options.X_value); k++) {
+          for (var l = 0; l < (h + Math.abs(options.Y_value)); l++) {
+            let val = (((k - w) / options.X_value) * 255);
+            pixelSetter(k, l, [val, val, val, 255], newPixels);
+          }
+        }
+        for (var k = 0; k < (w + options.X_value); k++) {
+          for (var l = 0; l < Math.abs(options.Y_value); l++) {
+            if (l < ((options.Y_value / options.X_value) * (options.X_value + w - k)) && k > w) {
+              continue;
+            }
+            let val = 255 - (l / Math.abs(options.Y_value) * 255);
+            pixelSetter(k, l, [val, val, val, 255], newPixels);
+          }
+        }
+        for (let i = 0; i < iMax && i < w; i++) {
+          for (let j = 0; j < jMax && j < h; j++) {
+            let x = i, y = j + Math.abs(options.Y_value);
+            pixelSetter(x, y, [pixels.get(i, j, 0), pixels.get(i, j, 1), pixels.get(i, j, 2), pixels.get(i, j, 3)], newPixels);
+          }
         }
       }
       return newPixels;

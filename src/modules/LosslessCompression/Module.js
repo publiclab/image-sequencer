7faad06f443892fe93ba948e3c13e9baad467dac
daemon1024 <barun1024@gmail.com>
2020-01-31 00:45:39 +0530

    Implement required changes for lossless compression module

--- src/modules/LosslessCompression/Module.js
+++ src/modules/LosslessCompression/Module.js
@@ -1,16 +1,44 @@
 module.exports = function LosslessCompress(options, UI) {
-  const compress_images = require("compress-images");
-  compress_images(
-    "src/img/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}",
-    "build/img/",
-    { compress_force: false, statistic: true, autoupdate: true },
-    false,
-    { jpg: { engine: "jpegoptim", command: ["--all-progressive", "-d"] } },
-    { png: { engine: "optipng" } },
-    function(err, completed) {
-      if (completed === true) {
-        // Doing something.
+  function draw(input, callback, progressObj) {
+    const compress_images = require("compress-images");
+    compress_images(
+      "images/**/*.{jpg,JPG,jpeg,JPEG,png}",
+      "results/",
+      { compress_force: false, statistic: true, autoupdate: true },
+      false,
+      { jpg: { engine: "jpegoptim", command: ["--all-progressive", "-d"] } },
+      { png: { engine: "optipng" } },
+      function(err, completed) {
+        if (completed === true) {
+          var reader = new FileReader();
+          reader.readAsDataURL("results/**/*");
+          reader.onloadend = function() {
+            base64data = reader.result;
+            output(null, base64data, input.format, false);
+            if (callback) callback();
+            return;
+          };
+        }
       }
+    );
+    progressObj.stop(true);
+    progressObj.overrideFlag = true;
+
+    var step = this;
+
+    function output(image, datauri, mimetype, wasmSuccess) {
+      step.output = {
+        src: datauri,
+        format: mimetype,
+        wasmSuccess,
+        useWasm: options.useWasm
+      };
     }
-  );
+  }
+  return {
+    options: options,
+    draw: draw,
+    output: output,
+    UI: UI
+  };
 };

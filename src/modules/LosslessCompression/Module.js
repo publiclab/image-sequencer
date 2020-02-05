f34f8fbcd611f8399c01e5dedcdc5d488a295e50
daemon1024 <barun1024@gmail.com>
2020-02-05 23:26:51 +0530

    Add docs link and revert unecessary changes

--- src/modules/LosslessCompression/Module.js
+++ src/modules/LosslessCompression/Module.js
@@ -1,17 +1,17 @@
 module.exports = function LosslessCompress(options, UI) {
   function draw(input, callback, progressObj) {
-    const compress_images = require("compress-images");
+    const compress_images = require('compress-images');
     compress_images(
-      "images/**/*.{jpg,JPG,jpeg,JPEG,png}",
-      "results/",
+      'input',
+      'results/',
       { compress_force: false, statistic: true, autoupdate: true },
       false,
-      { jpg: { engine: "jpegoptim", command: ["--all-progressive", "-d"] } },
-      { png: { engine: "optipng" } },
+      { jpg: { engine: 'jpegoptim', command: ['--all-progressive', '-d'] } },
+      { png: { engine: 'optipng' } },
       function(err, completed) {
         if (completed === true) {
           var reader = new FileReader();
-          reader.readAsDataURL("results/**/*");
+          reader.readAsDataURL('results/**/*');
           reader.onloadend = function() {
             base64data = reader.result;
             output(null, base64data, input.format, false);

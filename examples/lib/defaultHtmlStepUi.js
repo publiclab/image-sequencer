2707ec94f21451f9d8a5859fd8e13b5c6cc20051
ashirbad29 <46968256+ashirbad29@users.noreply.github.com>
2020-09-28 21:13:59 +0530

    Load image docs link removed

--- examples/lib/defaultHtmlStepUi.js
+++ examples/lib/defaultHtmlStepUi.js
@@ -48,7 +48,7 @@ function DefaultHtmlStepUi(_sequencer, options) {
               <div class="row step">\
                 <div class="col-md-4 details container-fluid">\
                   <div class="cal collapse in"><p>' +
-                    '<a href="https://github.com/publiclab/image-sequencer/blob/main/docs/MODULES.md#' + step.name + '-module">' + (step.description || '') + '</a>' +
+                    '<a ' + (step.name === "load-image" || 'href="https://github.com/publiclab/image-sequencer/blob/main/docs/MODULES.md#') + step.name + '-module">' + (step.description || '') + '</a>' +
                  '</p></div>\
                 </div>\
                 <div class="col-md-8 cal collapse in step-column">\

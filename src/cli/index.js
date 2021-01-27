5451fcb472e8c9dbdc5e0da998475ae06def62fc
daemon1024 <barun1024@gmail.com>
2021-01-27 19:35:43 +0000

    refactor

--- src/cli/index.js
+++ src/cli/index.js
@@ -13,66 +13,61 @@ function exit(message) {
   process.exit(1);
 }
 
-function executeCli(program) {
-  if (program.saveSequence) saveSequence(program, sequencer);
-  else if (program.installModule) installModule(program, sequencer);
-  else {
-    // Parse step into an array to allow for multiple steps.
-    if (!program.step) exit('No steps passed');
-    program.step = program.step.split(' ');
+function parseSteps(program) {
+  // Parse step into an array to allow for multiple steps.
+  if (!program.step) exit('No steps passed');
+  program.step = program.step.split(' ');
 
-    // User must input an image.
-    if (!program.image) exit('Can\'t read file.');
+  // User must input an image.
+  if (!program.image) exit('Can\'t read file.');
 
-    // User must input an image.
-    fs.access(program.image, function (err) {
-      if (err) exit('Can\'t read file.');
-    });
+  // User must input an image.
+  fs.access(program.image, function (err) {
+    if (err) exit('Can\'t read file.');
+  });
 
-    // User must input a step. If steps exist, check that every step is a valid step.
-    if (!program.step || !utils.validateSteps(program.step, sequencer))
-      exit('Please ensure all steps are valid.');
+  // User must input a step. If steps exist, check that every step is a valid step.
+  if (!program.step || !utils.validateSteps(program.step, sequencer))
+    exit('Please ensure all steps are valid.');
 
-    // If there's no user defined output directory, select a default directory.
-    program.output = program.output || './output/';
+  // If there's no user defined output directory, select a default directory.
+  program.output = program.output || './output/';
 
-    // Set sequencer to log module outputs, if any.
-    sequencer.setUI({
-      onComplete: function (step) {
-        // Get information of outputs.
-        step.info = sequencer.modulesInfo(step.name);
+  // Set sequencer to log module outputs, if any.
+  sequencer.setUI({
+    onComplete: function (step) {
+      // Get information of outputs.
+      step.info = sequencer.modulesInfo(step.name);
 
-        for (var output in step.info.outputs) {
-          console.log(
-            '[' + program.step + ']: ' + output + ' = ' + step[output]
-          );
-        }
-      },
-      notify: function (msg) {
-        console.log('\x1b[36m%s\x1b[0m', '🌟  ' + msg);
-      },
-    });
-
-    // Finally, if everything is alright, load the image, add the steps and run the sequencer.
-    sequencer.loadImages(program.image, function () {
-      console.warn(
-        '\x1b[33m%s\x1b[0m',
-        'Please wait \n output directory generated will be empty until the execution is complete'
-      );
-
-      //Generate the Output Directory
-      var outputFilename = program.output.split('/').slice(-1)[0];
-      if (outputFilename.includes('.')) {
-        // user did give an output filename we have to remove it from dir
-        program.output = program.output.split('/').slice(0, -1).join('/');
-      } else {
-        outputFilename = null;
+      for (var output in step.info.outputs) {
+        console.log('[' + program.step + ']: ' + output + ' = ' + step[output]);
       }
+    },
+    notify: function (msg) {
+      console.log('\x1b[36m%s\x1b[0m', '🌟  ' + msg);
+    },
+  });
 
-      sequencerSteps(program, sequencer, outputFilename);
-    });
-  }
+  // Finally, if everything is alright, load the image, add the steps and run the sequencer.
+  sequencer.loadImages(program.image, function () {
+    console.warn(
+      '\x1b[33m%s\x1b[0m',
+      'Please wait \n output directory generated will be empty until the execution is complete'
+    );
+
+    //Generate the Output Directory
+    var outputFilename = program.output.split('/').slice(-1)[0];
+    if (outputFilename.includes('.')) {
+      // user did give an output filename we have to remove it from dir
+      program.output = program.output.split('/').slice(0, -1).join('/');
+    } else {
+      outputFilename = null;
+    }
+
+    sequencerSteps(program, sequencer, outputFilename);
+  });
 }
+
 function cli(args) {
   program
     .version('0.1.0')
@@ -91,7 +86,9 @@ function cli(args) {
     )
     .parse(args);
 
-  executeCli(program);
+  if (program.saveSequence) saveSequence(program, sequencer);
+  else if (program.installModule) installModule(program, sequencer);
+  else parseSteps(program);
 }
 
 module.exports = cli;

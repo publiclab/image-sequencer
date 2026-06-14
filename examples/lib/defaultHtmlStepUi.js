50c31916092695b719e92facf46ff97ead13b44c
faratabassum09-a11y <fara.tabassum09@gmail.com>
2026-06-14 20:24:51 +0530

    fix: hide docs link for load-image step - fixes #1525

--- examples/lib/defaultHtmlStepUi.js
+++ examples/lib/defaultHtmlStepUi.js
@@ -51,7 +51,7 @@ function DefaultHtmlStepUi(_sequencer, options) {
               <div class="row step">\
                 <div class="col-md-4 details container-fluid">\
                   <div class="cal collapse in"><p>' +
-                    '<a href="' + stepDocsLink + '">' + (step.description || '') + '</a>' +
+                    (stepDocsLink ? '<a href="' + stepDocsLink + '">' + (step.description || '') + '</a>' : (step.description || '')) +
                  '</p></div>\
                 </div>\
                 <div class="col-md-8 cal collapse in step-column">\
